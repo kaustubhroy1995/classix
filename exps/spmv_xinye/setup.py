b4257806e2413ed2c7fb6bde60c4fc380dc593f7
chenxinye <cclcquant@yahoo.com>
2026-01-24 22:56:30 +0100

    fix spmv setup.py

--- exps/spmv_xinye/setup.py
+++ exps/spmv_xinye/setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, Extension
-import numpy as np
+from setuptools.command.build_ext import build_ext
+
+class build_ext_with_numpy(build_ext):
+    def finalize_options(self):
+        super().finalize_options()
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 spmv_module = Extension(
     'spmv',
     sources=['spmv.c'],
-    include_dirs=[np.get_include()], 
-    extra_compile_args=['-O3', '-march=native']
+    extra_compile_args=['-O3', '-march=native'],
 )
 
 setup(
     name='spmv',
     version='2.0',
     description='Sparse submatrix-vector multiplication for CLASSIX_T',
-    ext_modules=[spmv_module]
+    ext_modules=[spmv_module],
+    setup_requires=['numpy'],
+    cmdclass={'build_ext': build_ext_with_numpy},
 )
--- exps/spmv_xinye/test_spmv.py
+++ exps/spmv_xinye/test_spmv.py
@@ -9,4 +9,4 @@ v = np.array([1.0, 1.0, 1.0])
 result = np.zeros(3, dtype=np.float64)
 
 spsubmatxvec(A_sparse.data, A_sparse.indptr, A_sparse.indices, 0, 3, v, result)
-print("Result:", result)  
+print("Result:", result)  # Expected output: [ 3.  7. 11.]
