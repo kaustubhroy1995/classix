f59e388910d5f16556b7e06ea4ee028bd45e4fd7
the-null <cclcquant@yahoo.com>
2026-01-25 21:23:09 +0100

    Refactor setup.py to use Cython and platform-specific args
    Updated setup.py to include Cython for building extensions and adjusted compiler arguments based on the platform.

--- setup.py
+++ setup.py
@@ -1,39 +1,50 @@
 # setup.py
+import sys
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import numpy as np
+from Cython.Build import cythonize
+
 
 class CustomBuildExt(build_ext):
-    def run(self):
+    def finalize_options(self):
+        super().finalize_options()
+        import numpy as np
         self.include_dirs.append(np.get_include())
-        super().run()
+
+
+if sys.platform == "win32":
+    c_args = ["/O2"]
+else:
+    c_args = ["-O3"]
+
 
 ext_modules = [
-    Extension("classix.aggregate_ed_c", ["classix/aggregate_ed_c.pyx"]),
-    Extension("classix.aggregate_ed_cm", ["classix/aggregate_ed_cm.pyx"]),
-    Extension("classix.merge_ed_cm", ["classix/merge_ed_cm.pyx"]),
-    Extension("classix.merge_ed_cm_win", ["classix/merge_ed_cm_win.pyx"]),
-    
-    Extension("classix.aggregate_md_cm", ["classix/aggregate_md_cm.pyx"]),
-    Extension("classix.merge_md_cm", ["classix/merge_md_cm.pyx"]),
-    Extension(
-            "classix.aggregate_td_cm",
-            sources=["classix/aggregate_td_cm.pyx"],
-            language="c++",  # This is the critical line
-            include_dirs=[np.get_include()],
-            extra_compile_args=["-O3"]
-        ),
-    Extension("classix.merge_td_cm", ["classix/merge_td_cm.pyx"]),
+    Extension("classix.aggregate_ed_c", ["classix/aggregate_ed_c.pyx"], extra_compile_args=c_args),
+    Extension("classix.aggregate_ed_cm", ["classix/aggregate_ed_cm.pyx"], extra_compile_args=c_args),
+    Extension("classix.merge_ed_cm", ["classix/merge_ed_cm.pyx"], extra_compile_args=c_args),
+    Extension("classix.merge_ed_cm_win", ["classix/merge_ed_cm_win.pyx"], extra_compile_args=c_args),
+
+    Extension("classix.aggregate_md_cm", ["classix/aggregate_md_cm.pyx"], extra_compile_args=c_args),
+    Extension("classix.merge_md_cm", ["classix/merge_md_cm.pyx"], extra_compile_args=c_args),
 
     Extension(
-        "spmv",                          # 模組名：import spmv
-        sources=["classix/spmv.c"],              #（在項目根目錄或 classix/ 下）
-        include_dirs=[np.get_include()], # numpy 頭文件（如果需要 array 交互）
-        extra_compile_args=['-O3']       # 優化
+        "classix.aggregate_td_cm",
+        ["classix/aggregate_td_cm.pyx"],
+        language="c++",
+        extra_compile_args=c_args,
     ),
+
+    Extension("classix.merge_td_cm", ["classix/merge_td_cm.pyx"], extra_compile_args=c_args),
+
+    Extension("spmv", ["classix/spmv.pyx"], extra_compile_args=c_args),
 ]
 
+
 setup(
-    ext_modules=ext_modules,
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={"language_level": "3"},
+        annotate=False,
+    ),
     cmdclass={"build_ext": CustomBuildExt},
 )
