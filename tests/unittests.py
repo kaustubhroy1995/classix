b26c52a1f75f267089fe6c75db445c747ef70f52
chenxinye <cclcquant@yahoo.com>
2026-01-25 13:48:19 +0100

    fix unittests

--- tests/unittests.py
+++ tests/unittests.py
@@ -235,7 +235,7 @@ class TestClassix(unittest.TestCase):
 
             clx.explain(plot=False, showsplist=True, figsize=(10,10),  savefig=True)
             clx.explain(0,  plot=False, savefig=True, showsplist=True)
-            clx.form_starting_point_clusters_table(aggregate=True)
+            clx.form_starting_point_clusters_table(data=clx.data, aggregate=True)
             clx.explain(3, 2000,  plot=False, savefig=False)
             clx.explain(0, 2008,  plot=False, savefig=True, replace_name=['Superman', 'Batman'])
 
